3534e5bc5167013e09bdd8e315ed1f1fbe8dd873
LauBok <bl226@algebra2.stat.duke.edu>
2021-07-06 15:57:38 -0400

    add test case 1

---
+++ test/no_covariates/test1.R
@@ -0,0 +1,38 @@
+# Test 1: No covariates, 2 strata (n - 00, c - 01), ER for 00.
+# July 6, 2021
+# Models:
+# P(S = 00) = 0.3
+# P(S = 01) = 0.7
+# Y | S = 00 ~ N(3, 1)
+# Y | S = 01, Z ~ N(-1 - Z, 2)
+
+set.seed(0)
+df <- datasets::infert
+N <- nrow(df)
+
+# Generate Z, S, Y, D
+df$Z <- rbinom(N, 1, 0.5)
+df$S <- sample(c(0, 1), size = N, replace = T, prob = c(0.3, 0.7))
+df$D <- ifelse(df$S == 3 | df$S + df$Z == 2, 1, 0)
+df$Y[df$S == 0] <- rnorm(sum(df$S == 0), 3, 1)
+df$Y[df$S == 1] <- rnorm(sum(df$S == 1), -1 - df$Z, 2)
+
+# PS
+result <- PS(
+  S.formula = Z + D ~ 1,
+  Y.formula = Y ~ 1,
+  Y.family = gaussian(),
+  data = df,
+  monotonicity = "strong",
+  ER = "00",
+  trunc = FALSE,
+  chains = 1, warmup = 200, iter = 500
+)
+
+tmp <- PSobject(
+  S.formula = Z + D ~ 1,
+  Y.formula = Y ~ 1,
+  Y.family = gaussian(),
+  monotonicity = "strong",
+  ER = "00"
+)
