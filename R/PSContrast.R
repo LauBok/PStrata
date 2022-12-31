af68ef72e65dcda3f1ca1060c89a8fe36125d698
Bo Liu <bl226@duke.edu>
2022-12-30 20:30:27 -0500

    add PSContrast

---
+++ R/PSContrast.R
@@ -0,0 +1,161 @@
+#' Contrast of potential outcome for principal stratification analysis
+#' 
+#' Create an object that represents contrast of potential outcomes by treatment arms,
+#' strata or time points.
+#' 
+#' @param outcome an object of class \code{PSoutcome} or \code{PSContrast}
+#' @param S a vector denoting which strata to take contrasts. Default is \code{NULL} indicating
+#' no contrasts are taken. Set to `TRUE` to take contrasts between all strata.
+#' @param Z a vector denoting which treatment arms to take contrasts. Default is \code{NULL} indicating
+#' no contrasts are taken. Set to `TRUE` to take contrasts between all treatment arms.
+#' @param T a vector denoting which time points to take contrasts. Default is \code{NULL} indicating
+#' no contrasts are taken. Set to `TRUE` to take contrasts between all time points. This is used
+#' only when `object` is obtained under survival outcome.
+#' @param type Either \code{"all"} (default), \code{"sequential"} or \code{"cycle"}.
+#' If \code{"all"}, every pairwise contrasts are taken.
+#' If \code{"sequential"}, contrasts are taken over every consecutive pairs.
+#' If \code{"cycle"}, contrasts are taken over every consecutive pairs and also between the
+#' first and the last levels.
+#' 
+#' @export
+PSContrast <- function(outcome, S = NULL, Z = NULL, T = NULL, 
+                       type = c("all", "sequential", "cycle")) {
+  old_outcome_array <- outcome$outcome_array
+  type <- match.arg(type, c("all", "sequential", "cycle"))
+  if (outcome$is.survival == F) {
+    if (!is.null(S)) {
+      dim_outcome_array <- dim(old_outcome_array)
+      name_outcome_array <- dimnames(old_outcome_array)
+      levels <- (1:dim_outcome_array[1])[S]
+      if (type == "all")
+        contrast_levels <- (function(x) x[x[,1] > x[,2], ])(expand.grid(levels, levels))
+      if (type == "sequential" || (type == "cycle" && length(levels) == 2))
+        contrast_levels <- data.frame(Var1 = levels[-1], Var2 = levels[-length(levels)])
+      if (type == "cycle" && length(levels) > 2) {
+        contrast_levels <- data.frame(Var1 = c(levels[-1], levels[1]), Var2 = levels)
+      }
+      dim_outcome_array[1] <- nrow(contrast_levels)
+      name_outcome_array[[1]] <- apply(contrast_levels, 1, function(x) {
+        paste0("{", name_outcome_array[[1]][x[1]], "}-{", name_outcome_array[[1]][x[2]], "}")
+      })
+      new_outcome_array <- array(NA, dim = dim_outcome_array, dimnames = name_outcome_array)
+      for (i in 1:nrow(contrast_levels)) {
+        new_outcome_array[i, , ] <- old_outcome_array[contrast_levels[i, 1], ,] - 
+          old_outcome_array[contrast_levels[i, 2], ,]
+      }
+      old_outcome_array <- new_outcome_array
+    }
+    if (!is.null(Z)) {
+      dim_outcome_array <- dim(old_outcome_array)
+      name_outcome_array <- dimnames(old_outcome_array)
+      levels <- (1:dim_outcome_array[2])[Z]
+      if (type == "all")
+        contrast_levels <- (function(x) x[x[,1] > x[,2], ])(expand.grid(levels, levels))
+      if (type == "sequential" || (type == "cycle" && length(levels) == 2))
+        contrast_levels <- data.frame(Var1 = levels[-1], Var2 = levels[-length(levels)])
+      if (type == "cycle" && length(levels) > 2) {
+        contrast_levels <- data.frame(Var1 = c(levels[-1], levels[1]), Var2 = levels)
+      }
+      dim_outcome_array[2] <- nrow(contrast_levels)
+      name_outcome_array[[2]] <- apply(contrast_levels, 1, function(x) {
+        paste0("{", name_outcome_array[[2]][x[1]], "}-{", name_outcome_array[[2]][x[2]], "}")
+      })
+      new_outcome_array <- array(NA, dim = dim_outcome_array, dimnames = name_outcome_array)
+      for (i in 1:nrow(contrast_levels)) {
+        new_outcome_array[, i, ] <- old_outcome_array[, contrast_levels[i, 1] ,] - 
+          old_outcome_array[, contrast_levels[i, 2], ]
+      }
+      old_outcome_array <- new_outcome_array
+    }
+  } else {
+    if (!is.null(S)) {
+      dim_outcome_array <- dim(old_outcome_array)
+      name_outcome_array <- dimnames(old_outcome_array)
+      levels <- (1:dim_outcome_array[1])[S]
+      if (type == "all")
+        contrast_levels <- (function(x) x[x[,1] > x[,2], ])(expand.grid(levels, levels))
+      if (type == "sequential" || (type == "cycle" && length(levels) == 2))
+        contrast_levels <- data.frame(Var1 = levels[-1], Var2 = levels[-length(levels)])
+      if (type == "cycle" && length(levels) > 2) {
+        contrast_levels <- data.frame(Var1 = c(levels[-1], levels[1]), Var2 = levels)
+      }
+      dim_outcome_array[1] <- nrow(contrast_levels)
+      name_outcome_array[[1]] <- apply(contrast_levels, 1, function(x) {
+        paste0("{", name_outcome_array[[1]][x[1]], "}-{", name_outcome_array[[1]][x[2]], "}")
+      })
+      new_outcome_array <- array(NA, dim = dim_outcome_array, dimnames = name_outcome_array)
+      for (i in 1:nrow(contrast_levels)) {
+        new_outcome_array[i, , ,] <- old_outcome_array[contrast_levels[i, 1], , ,] - 
+          old_outcome_array[contrast_levels[i, 2], , ,]
+      }
+      old_outcome_array <- new_outcome_array
+    }
+    if (!is.null(Z)) {
+      dim_outcome_array <- dim(old_outcome_array)
+      name_outcome_array <- dimnames(old_outcome_array)
+      levels <- (1:dim_outcome_array[2])[Z]
+      if (type == "all")
+        contrast_levels <- (function(x) x[x[,1] > x[,2], ])(expand.grid(levels, levels))
+      if (type == "sequential" || (type == "cycle" && length(levels) == 2))
+        contrast_levels <- data.frame(Var1 = levels[-1], Var2 = levels[-length(levels)])
+      if (type == "cycle" && length(levels) > 2) {
+        contrast_levels <- data.frame(Var1 = c(levels[-1], levels[1]), Var2 = levels)
+      }
+      dim_outcome_array[2] <- nrow(contrast_levels)
+      name_outcome_array[[2]] <- apply(contrast_levels, 1, function(x) {
+        paste0("{", name_outcome_array[[2]][x[1]], "}-{", name_outcome_array[[2]][x[2]], "}")
+      })
+      new_outcome_array <- array(NA, dim = dim_outcome_array, dimnames = name_outcome_array)
+      for (i in 1:nrow(contrast_levels)) {
+        new_outcome_array[, i, , ] <- old_outcome_array[, contrast_levels[i, 1] , ,] - 
+          old_outcome_array[, contrast_levels[i, 2], , ]
+      }
+      old_outcome_array <- new_outcome_array
+    }
+    if (!is.null(T)) {
+      dim_outcome_array <- dim(old_outcome_array)
+      name_outcome_array <- dimnames(old_outcome_array)
+      levels <- (1:dim_outcome_array[3])[T]
+      if (type == "all")
+        contrast_levels <- (function(x) x[x[,1] > x[,2], ])(expand.grid(levels, levels))
+      if (type == "sequential" || (type == "cycle" && length(levels) == 2))
+        contrast_levels <- data.frame(Var1 = levels[-1], Var2 = levels[-length(levels)])
+      if (type == "cycle" && length(levels) > 2) {
+        contrast_levels <- data.frame(Var1 = c(levels[-1], levels[1]), Var2 = levels)
+      }
+      dim_outcome_array[3] <- nrow(contrast_levels)
+      name_outcome_array[[3]] <- apply(contrast_levels, 1, function(x) {
+        paste0("{", name_outcome_array[[3]][x[1]], "}-{", name_outcome_array[[3]][x[2]], "}")
+      })
+      new_outcome_array <- array(NA, dim = dim_outcome_array, dimnames = name_outcome_array)
+      for (i in 1:nrow(contrast_levels)) {
+        new_outcome_array[, , i, ] <- old_outcome_array[, , contrast_levels[i, 1] ,] - 
+          old_outcome_array[, , contrast_levels[i, 2], ]
+      }
+      old_outcome_array <- new_outcome_array
+    }
+  }
+  return (structure(
+    list(
+      outcome_array = old_outcome_array,
+      is.survival = outcome$is.survival,
+      time_points = outcome$time_points
+    ),
+    class = c("PSContrast", "PSOutcome")
+  ))
+}
+
+#' @export 
+print.PSContrast <- function(PSoutcome) {
+  if (!PSoutcome$is.survival) {
+    cat("Non-survival PSContrast Object (", dim(PSoutcome$outcome_array)[1],
+        " strata, ", dim(PSoutcome$outcome_array)[2], 
+        " treatment arms, ", dim(PSoutcome$outcome_array)[3], " iterations)\n", sep = '')
+  } else {
+    cat("Survival PSContrast Object (", dim(PSoutcome$outcome_array)[1],
+        " strata, ", dim(PSoutcome$outcome_array)[2], 
+        " treatment arms, ", dim(PSoutcome$outcome_array)[4], " iterations)\n", sep = '')
+    cat("Evaluated at ", dim(PSoutcome$outcome_array)[3], " time points.\n", sep = '')
+  }
+}
+  
--- R/PSOutcome.R
+++ R/PSOutcome.R
@@ -34,7 +34,37 @@ PSOutcome <- function(PStrataObj) {
       class = "PSOutcome"
     ))
   }
+  else {
+    mean_surv_prob <- rstan::extract(PStrataObj$post_samples)$'mean_surv_prob'
+    S_count <- PStrataObj$PSobject$strata_info$num_strata
+    Z_count <- PStrataObj$PSobject$strata_info$num_treatment
+    time_points <- make_standata(PStrataObj$PSobject)$time
+    T_count <- length(time_points)
+    iter_count <- dim(mean_surv_prob)[1]
+    SZDG_table <- PStrataObj$PSobject$SZDG_table
     
+    outcome_array <- array(NA, dim = c(S_count, Z_count, T_count, iter_count))
+    for (S in 1:S_count) {
+      for (Z in 1:Z_count) {
+        G <- SZDG_table[SZDG_table[, 'S'] == S - 1 & SZDG_table[, 'Z'] == Z - 1, 'G']
+        outcome_array[S, Z, , ] <- t(mean_surv_prob[, G, ])
+      }
+    }
+    
+    
+    dimnames(outcome_array)[[1]] <- PStrataObj$PSobject$strata_info$strata_names
+    dimnames(outcome_array)[[2]] <- PStrataObj$PSobject$Z_names
+    dimnames(outcome_array)[[3]] <- 1:T_count
+    
+    return (structure(
+      list(
+        outcome_array = outcome_array,
+        is.survival = T,
+        time_points = time_points
+      ),
+      class = "PSOutcome"
+    ))
+  }
 }
 
 #' @exportS3Method 
@@ -43,6 +73,11 @@ print.PSOutcome <- function(PSoutcome) {
     cat("Non-survival PSOutcome Object (", dim(PSoutcome$outcome_array)[1],
         " strata, ", dim(PSoutcome$outcome_array)[2], 
         " treatment arms, ", dim(PSoutcome$outcome_array)[3], " iterations)\n", sep = '')
+  } else {
+    cat("Survival PSOutcome Object (", dim(PSoutcome$outcome_array)[1],
+        " strata, ", dim(PSoutcome$outcome_array)[2], 
+        " treatment arms, ", dim(PSoutcome$outcome_array)[4], " iterations)\n", sep = '')
+    cat("Evaluated at ", dim(PSoutcome$outcome_array)[3], " time points.\n", sep = '')
   }
 }
 
@@ -68,6 +103,8 @@ summary.PSOutcome <- function(PSoutcome, type = c("array", "matrix", "data.frame
     rownames(summary_res_matrix) <- apply(tmp_names, 1, paste, collapse = ":")
     
     summary_res_df <- as.data.frame(summary_res_matrix)
+    SZ <- purrr::transpose(stringr::str_split(rownames(summary_res_df), ":"))
+    summary_res_df <- cbind(S = unlist(SZ[[1]]), Z = unlist(SZ[[2]]), summary_res_df)
     
     if (match.arg(type, c("array", "matrix", "data.frame")) == "array")
       return (summary_res_array)
@@ -76,7 +113,46 @@ summary.PSOutcome <- function(PSoutcome, type = c("array", "matrix", "data.frame
     else if (match.arg(type, c("array", "matrix", "data.frame")) == "data.frame")
       return (summary_res_df)
   }
-  
+  else {
+    summary_raw <- apply(PSoutcome$outcome_array, c(3,2,1), 
+                         function(x) c(mean(x), sd(x), quantile(x, 0.025), quantile(x,0.25),
+                                       median(x), quantile(x, 0.75), quantile(x, 0.975)))
+    summary_res_array <- aperm(summary_raw, c(4,3,2,1))
+    summary_names <- c("mean", "sd", "2.5%", "25%", "median", "75%", "97.5%")
+    S_names <- dimnames(summary_res_array)[[1]]
+    Z_names <- dimnames(summary_res_array)[[2]]
+    T_names <- dimnames(summary_res_array)[[3]]
+    dimnames(summary_res_array)[[4]] <- summary_names
+    summary_res_matrix <- matrix(nrow = length(S_names) * length(Z_names) * length(T_names), 
+                                 ncol = length(summary_names))
+    for (i in 1:dim(summary_res_array)[1])
+      for (j in 1:dim(summary_res_array)[2]) {
+        for (t in 1:dim(summary_res_array)[3]) {
+          summary_res_matrix[(i - 1) * dim(summary_res_array)[2] * dim(summary_res_array)[3] + (j - 1) * dim(summary_res_array)[3] + t, ] <- summary_res_array[i, j, t, ]
+        }
+      }
+    tmp_names <- expand.grid(T_names, Z_names, S_names)[, c(3,2,1)]
+    colnames(summary_res_matrix) <- summary_names
+    rownames(summary_res_matrix) <- apply(tmp_names, 1, paste, collapse = ":")
+    
+    summary_res_df <- as.data.frame(summary_res_matrix)
+    SZT <- purrr::transpose(stringr::str_split(rownames(summary_res_df), ":"))
+    if (any(is.na(as.integer(unlist(SZT[[3]]))))){
+      summary_res_df <- cbind(S = unlist(SZT[[1]]), Z = unlist(SZT[[2]]), 
+                       T = unlist(SZT[[3]]), summary_res_df)
+    }
+    else{
+      summary_res_df <- cbind(S = unlist(SZT[[1]]), Z = unlist(SZT[[2]]), 
+                       T = PSoutcome$time_points[as.integer(unlist(SZT[[3]]))], summary_res_df)
+    }
+    
+    if (match.arg(type, c("array", "matrix", "data.frame")) == "array")
+      return (summary_res_array)
+    else if (match.arg(type, c("array", "matrix", "data.frame")) == "matrix")
+      return (summary_res_matrix)
+    else if (match.arg(type, c("array", "matrix", "data.frame")) == "data.frame")
+      return (summary_res_df)
+  }
 }
 
 #' @export
@@ -90,6 +166,8 @@ summary.PSOutcome <- function(PSoutcome, type = c("array", "matrix", "data.frame
       PSoutcome$outcome_array <- PSoutcome$outcome_array[S, Z, iter, drop = FALSE]
     else
       PSoutcome$outcome_array <- PSoutcome$outcome_array[S, Z, T, drop = FALSE]
+  } else {
+    PSoutcome$outcome_array <- PSoutcome$outcome_array[S, Z, T, iter, drop = FALSE]
   }
   
   return (PSoutcome)
@@ -99,12 +177,28 @@ summary.PSOutcome <- function(PSoutcome, type = c("array", "matrix", "data.frame
 plot.PSOutcome <- function(PSoutcome, se = T) {
   if (!PSoutcome$is.survival) {
     plot_df <- summary(PSoutcome, "data.frame")
-    SZ <- purrr::transpose(stringr::str_split(rownames(plot_df), ":"))
-    plot_df <- cbind(S = unlist(SZ[[1]]), Z = unlist(SZ[[2]]), plot_df)
     Gplot <- ggplot2::ggplot(plot_df) + ggplot2::geom_point(ggplot2::aes(x = mean, y = "")) + 
       ggplot2::facet_grid(Z~S, scale = "free")
     if (se)
       Gplot <- Gplot + ggplot2::geom_linerange(ggplot2::aes(xmin = `2.5%`, xmax = `97.5%`, y = ""))
+    return (Gplot)
+  }
+  else {
+    plot_df <- summary(PSoutcome, "data.frame")
+    if (any(is.na(as.integer(plot_df$T)))) {
+      Gplot <- ggplot2::ggplot(plot_df) + ggplot2::geom_point(ggplot2::aes(x = T, y = mean)) + 
+        ggplot2::facet_grid(Z~S, scale = "free")
+      if (se)
+        Gplot <- Gplot + ggplot2::geom_linerange(ggplot2::aes(ymin = `2.5%`, ymax = `97.5%`, x = T))
+      Gplot <- Gplot + ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, vjust = 0.5, hjust=1))
+    }
+    else{
+      Gplot <- ggplot2::ggplot(plot_df) + ggplot2::geom_line(ggplot2::aes(x = T, y = mean)) + 
+        ggplot2::facet_grid(Z~S, scale = "free")
+      if (se)
+        Gplot <- Gplot + ggplot2::geom_ribbon(ggplot2::aes(ymin = `2.5%`, ymax = `97.5%`, x = T),
+                                              alpha = 0.3)
+    }
     return (Gplot)
   }
 }
