8fb9c05acada55a58fb2f855a8955b25268f15e4
Bo Liu <bl226@duke.edu>
2023-11-04 01:20:33 -0400

    add RACE as a response

--- R/PSOutcome.R
+++ R/PSOutcome.R
@@ -12,7 +12,7 @@
 #' \item{time_points}{The time points at which the outcome is evaluated, if the outcome type is survival.}
 #' The S3 method \code{summary} and \code{plot} can be applied to the returned object.
 #' @export
-PSOutcome <- function(PStrataObj) {
+PSOutcome <- function(PStrataObj, type = c("probability", "RACE")) {
   if (inherits(PStrataObj, "PStrata")){
     mean_effect <- rstan::extract(PStrataObj$post_samples)$'mean_effect'
     S_count <- PStrataObj$PSobject$strata_info$num_strata
@@ -40,19 +40,25 @@ PSOutcome <- function(PStrataObj) {
     ))
   }
   else {
-    mean_surv_prob <- rstan::extract(PStrataObj$post_samples)$'mean_surv_prob'
+    outcome_type <- match.arg(type, c("probability", "RACE"))
+    if (outcome_type == "probability"){
+      mean_outcome <- rstan::extract(PStrataObj$post_samples)$'mean_surv_prob'
+    }
+    else if (outcome_type == "RACE") {
+      mean_outcome <- rstan::extract(PStrataObj$post_samples)$'mean_RACE'
+    }
     S_count <- PStrataObj$PSobject$strata_info$num_strata
     Z_count <- PStrataObj$PSobject$strata_info$num_treatment
     time_points <- make_standata(PStrataObj$PSobject)$time
     T_count <- length(time_points)
-    iter_count <- dim(mean_surv_prob)[1]
+    iter_count <- dim(mean_outcome)[1]
     SZDG_table <- PStrataObj$PSobject$SZDG_table
     
     outcome_array <- array(NA, dim = c(S_count, Z_count, T_count, iter_count))
     for (S in 1:S_count) {
       for (Z in 1:Z_count) {
         G <- SZDG_table[SZDG_table[, 'S'] == S - 1 & SZDG_table[, 'Z'] == Z - 1, 'G']
-        outcome_array[S, Z, , ] <- t(mean_surv_prob[, G, ])
+        outcome_array[S, Z, , ] <- t(mean_outcome[, G, ])
       }
     }
     
